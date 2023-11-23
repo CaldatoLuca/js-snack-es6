f590f4f615e06d42e317dde4e4f7fdfedad18393
CaldatoLuca <lucacaldato15@gmail.com>
2023-11-23 11:05:29 +0100

    correzzione

--- snack_2/js/main.js
+++ snack_2/js/main.js
@@ -41,20 +41,33 @@ const teams = [
 ];
 
 //creo un array con i valori aggiornati di punti e falli
-const updatedTeams = teams.map((team) => {
-  let { teamName, points, againstFouls } = team;
-  points = randomInt(0, 100);
-  againstFouls = randomInt(0, 10);
-  return { teamName, points, againstFouls };
+// const updatedTeams = teams.map((team) => {
+//   let { teamName, points, againstFouls } = team;
+//   points = randomInt(0, 100);
+//   againstFouls = randomInt(0, 10);
+//   return { teamName, points, againstFouls };
+// });
+// console.log(`Array aggiornato con punti fatti e falli subiti:`);
+// console.log(updatedTeams);
+
+teams.forEach((team) => {
+  // let { points, againstFouls } = team;
+  // points = randomInt(0, 100);
+  // againstFouls = randomInt(0, 100);
+  team.points = randomInt(0, 100);
+  team.againstFouls = randomInt(0, 10);
 });
-console.log(`Array aggiornato con punti fatti e falli subiti:`);
-console.log(updatedTeams);
+
+console.log(teams);
 
 //creo un array in cui non considero i punti fatti
-const noPointsTeams = updatedTeams.map((team) => {
-  let { teamName, againstFouls } = team;
+const noPointsTeams = teams.map((team) => {
+  const { teamName, againstFouls } = team;
 
   return { teamName, againstFouls };
 });
 console.log(`Array aggiornato senza punti fatti:`);
 console.log(noPointsTeams);
+
+//!non creare un nuovo array ma modifica quello in input
+//!se destrutturo le chiavi non si aggiorna il mio array
